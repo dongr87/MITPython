046f41d4b1e6b9a2f11f40432d850609637c41cd
dongr87 <mtwcore@gmail.com>
2019-04-29 23:57:24 -0400

    regex

--- boring/ch7_material.py
+++ boring/ch7_material.py
@@ -19,3 +19,24 @@ print('415-555-4242 is a phone number:')
 print(isPhoneNumber('415-555-4242'))
 print('Moshi moshi is a phone number:')
 print(isPhoneNumber('Moshi moshi'))
+
+# Find number from a text, by referencing isPhoneNumber function
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+    chunk = message[i:i + 12]
+    if isPhoneNumber(chunk):
+        print('Phone Number Found: ' + chunk)
+print('Done')
+
+# using regular expression
+import re
+
+# re.compile()
+# regexObj.search()
+# re.group() to store the found string patterns
+
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search(message)
+print(mo.group())
+# pipe for multiple matches
+# question mark for optional matching
